2b04089392d40740e41a8f3eed1ad62842464153
Daniel- <danielyg2904@gmail.com>
2025-08-09 00:36:50 +0700

    dapui ui adjustments, add fallback external terminal

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -7,11 +7,11 @@ return {
           elements = {
             {
               id = "scopes",
-              size = 0.25,
+              size = 0.45,
             },
             {
               id = "breakpoints",
-              size = 0.25,
+              size = 0.2,
             },
             {
               id = "stacks",
@@ -19,7 +19,7 @@ return {
             },
             {
               id = "watches",
-              size = 0.25,
+              size = 0.1,
             },
           },
           position = "left",
@@ -29,11 +29,11 @@ return {
           elements = {
             {
               id = "repl",
-              size = 0.2,
+              size = 0.4,
             },
             {
               id = "console",
-              size = 0.8,
+              size = 0.6,
             },
           },
           position = "bottom",
@@ -48,6 +48,29 @@ return {
       dap.listeners.after.event_initialized["dapui_config"] = function() end
       dap.listeners.before.event_terminated["dapui_config"] = function() end
       dap.listeners.before.event_exited["dapui_config"] = function() end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      -- Simple configuration to attach to remote java debug process
+      -- Taken directly from https://github.com/mfussenegger/nvim-dap/wiki/Java
+      local dap = require("dap")
+      -- 💡 Define how to launch the terminal for our app
+      -- This function will open a new Neovim tab and run the application there
+      dap.defaults.fallback.external_terminal = {
+        command = "wezterm", -- The command to execute WezTerm
+        args = { "start", "--cwd", vim.fn.getcwd() }, -- Arguments to launch WezTerm, including setting the current working directory
+      }
+      dap.configurations.java = {
+        {
+          type = "java",
+          request = "attach",
+          name = "Debug (Attach) - Remote",
+          hostName = "127.0.0.1",
+          port = 5005,
+        },
+      }
     end,
   },
 }
