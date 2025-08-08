4a6adf7f28e0110d98494cc2ab28a5d88c78006c
Daniel- <danielyg2904@gmail.com>
2025-08-09 00:36:49 +0700

    remove validate document from jdtls

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -1,11 +1,31 @@
 return {
-  "mfussenegger/nvim-jdtls",
-  opts = {
-    project_name = function(root_dir)
-      if root_dir == nil then
-        return vim.fn.getcwd()
-      end
-      return root_dir and vim.fs.basename(root_dir)
-    end,
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      project_name = function(root_dir)
+        if root_dir == nil then
+          return vim.fn.getcwd()
+        end
+        return root_dir and vim.fs.basename(root_dir)
+      end,
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts_extend = { "routes" },
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "lsp",
+            kind = "progress",
+            find = "Validate documents",
+          },
+          opts = {
+            skip = true,
+          },
+        },
+      },
+    },
   },
 }
