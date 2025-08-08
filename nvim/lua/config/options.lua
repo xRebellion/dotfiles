3cb563518a55e2bd0aa4cc6378307035fd6d71d9
Daniel- <danielyg2904@gmail.com>
2025-08-09 00:36:43 +0700

    disable shellslash don't know if this is needed?

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,12 @@ vim.o.swapfile = false
 vim.o.autoread = true
 vim.o.title = true
 vim.o.titlestring = "nvim - %t"
+-- Determine OS
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  vim.opt.shellslash = false -- Enable shellslash for Windows compatibility
+  vim.defer_fn(function()
+    vim.opt.shellslash = false
+  end, 5000)
+else
+  print("OS not found, defaulting to 'linux'")
+end
